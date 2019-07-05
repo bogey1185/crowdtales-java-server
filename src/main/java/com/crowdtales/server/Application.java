9cafdd89a5c23b821d02b3d89a55deb602147ef4
David Bogaerts <dbogaerts@gmail.com>
2019-07-05 14:10:38 -0500

    set up application class

---
+++ src/main/java/com/crowdtales/server/Application.java
@@ -0,0 +1,14 @@
+package com.crowdtales.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(Application.class, args);
+    }
+}
+
