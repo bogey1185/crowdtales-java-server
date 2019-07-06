92b877f09f962a71366a5214226174981f9ec2fd
David Bogaerts <dbogaerts@gmail.com>
2019-07-06 00:26:24 -0500

    user reg working including hash

--- src/main/java/com/crowdtales/server/User.java
+++ src/main/java/com/crowdtales/server/User.java
@@ -11,6 +11,7 @@ public class User {
 
     private String username;
     private String password;
+    private String password2;
     private String email;
 
     public int getId() { return id; }
@@ -21,6 +22,10 @@ public class User {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+
+    public String getPassword2() { return password2; }
+    public void setPassword2(String password2) { this.password2 = password2; }
+    public void setPassword2AsNull() { this.password2 = null; };
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
--- src/main/java/com/crowdtales/server/UserController.java
+++ src/main/java/com/crowdtales/server/UserController.java
@@ -1,12 +1,37 @@
 package com.crowdtales.server;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+
     @GetMapping("/users")
     public String getUsers() { return "HELLO WORLD"; }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) throws Exception {
+        String passwordOne = user.getPassword();
+        String passwordTwo = user.getPassword2();
+
+        if (passwordOne.equals(passwordTwo)) {
+            user.setEmail(user.getEmail().toLowerCase());
+            user.setPassword2AsNull();
+            User createdUser = userService.saveUser(user);
+            return createdUser;
+
+        } else throw new Exception("Passwords do not match");
+    }
 
 }
