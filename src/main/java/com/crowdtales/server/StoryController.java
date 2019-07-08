ec64bba7cd2795080e3fbf20201d277695b263a1
David Bogaerts <dbogaerts@gmail.com>
2019-07-07 23:37:55 -0500

    get single story and all stories working

--- src/main/java/com/crowdtales/server/StoryController.java
+++ src/main/java/com/crowdtales/server/StoryController.java
@@ -1,18 +1,27 @@
 package com.crowdtales.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.Optional;
 
 @RestController
 public class StoryController {
 
     @Autowired
     private StoryRepository storyRepository;
+
+    // Get all stories //
+
+    @GetMapping("/stories")
+    public Iterable<Story> getStories() { return storyRepository.findAll(); }
+
+    // Get specific story
+
+    @GetMapping("/stories/{id}")
+    public Optional<Story> getStoryById(@PathVariable int id) { return storyRepository.findById(id); }
 
     // Create story //
 
